6865abfecc97c0c09fb00bb4b56cdc0602411c3d
DayZmooN <karim-du01@live.fr>
2023-08-24 14:31:26 +0200

    effet scroll

--- asset/js/scroll.js
+++ asset/js/scroll.js
@@ -1,19 +1,50 @@
 document.addEventListener('DOMContentLoaded', function () {
-    let studentSection = document.querySelector('.container-card-student');
+    let container = document.querySelector('.container-card-student');
+    let body = document.body;
 
-    studentSection.addEventListener('scroll', function () {
-        // Détecter si la section .student est complètement défilée
-        if (studentSection.scrollTop + studentSection.clientHeight >= studentSection.scrollHeight) {
-            document.body.style.overflow = 'auto'; // Activer le défilement de la page
+    // Désactiver le défilement de la page
+    body.style.overflow = 'hidden';
+
+    // Initialiser AOS
+    AOS.init({
+        duration: 1000,
+        mirror: true,
+        container: container,
+        disable: 'mobile' // Désactiver les animations sur les appareils mobiles
+    });
+
+    // Écouter l'événement de défilement
+    container.addEventListener('scroll', function () {
+        applyScale();
+
+        // Si le conteneur est complètement défilé vers le haut ou vers le bas
+        if (container.scrollTop === 0 || container.scrollTop + container.clientHeight >= container.scrollHeight) {
+            body.style.overflow = 'auto'; // Activer le défilement de la page
         } else {
-            document.body.style.overflow = 'hidden'; // Désactiver le défilement de la page
+            body.style.overflow = 'hidden'; // Désactiver le défilement de la page
         }
     });
 
-    // Activer le défilement de la page si l'utilisateur n'est pas dans la section .student
-    document.addEventListener('scroll', function () {
-        if (window.scrollY < studentSection.offsetTop || window.scrollY > studentSection.offsetTop + studentSection.clientHeight) {
-            document.body.style.overflow = 'auto';
+    // Appliquer l'échelle initiale
+    applyScale();
+
+    // Fonction pour appliquer l'échelle
+    // Fonction pour appliquer l'échelle
+    function applyScale() {
+        // Vérifier si l'appareil est en mode mobile (par exemple, largeur inférieure à 768px)
+        if (window.innerWidth < 770) {
+            container.style.transform = 'scale(1)'; // Pas d'effet de zoom en mode mobile
+            body.style.overflow = 'auto'; // Activer le défilement de la page
+
+            return; // Quitter la fonction
         }
-    });
+
+        // Calculer le ratio de défilement
+        let scrollRatio = container.scrollTop / (container.scrollHeight - container.clientHeight);
+
+        // Appliquer une transformation d'échelle
+        let scaleValue = 1 + scrollRatio * 0.2; // Ajuster cette valeur
+        container.style.transform = `scale(${scaleValue})`;
+    }
+
 });
