4280569c8cc51bd2c67388be05baf1fff794214f
DayZmooN <karim-du01@live.fr>
2023-08-25 09:30:34 +0200

    animation scroll

--- asset/js/scroll.js
+++ asset/js/scroll.js
@@ -2,49 +2,28 @@ document.addEventListener('DOMContentLoaded', function () {
     let container = document.querySelector('.container-card-student');
     let body = document.body;
 
-    // Désactiver le défilement de la page
-    body.style.overflow = 'hidden';
-
-    // Initialiser AOS
-    AOS.init({
-        duration: 1000,
-        mirror: true,
-        container: container,
-        disable: 'mobile' // Désactiver les animations sur les appareils mobiles
-    });
-
-    // Écouter l'événement de défilement
-    container.addEventListener('scroll', function () {
-        applyScale();
-
-        // Si le conteneur est complètement défilé vers le haut ou vers le bas
-        if (container.scrollTop === 0 || container.scrollTop + container.clientHeight >= container.scrollHeight) {
-            body.style.overflow = 'auto'; // Activer le défilement de la page
-        } else {
-            body.style.overflow = 'hidden'; // Désactiver le défilement de la page
-        }
-    });
-
-    // Appliquer l'échelle initiale
+    container.addEventListener('scroll', onScroll);
     applyScale();
 
-    // Fonction pour appliquer l'échelle
-    // Fonction pour appliquer l'échelle
     function applyScale() {
-        // Vérifier si l'appareil est en mode mobile (par exemple, largeur inférieure à 768px)
         if (window.innerWidth < 770) {
-            container.style.transform = 'scale(1)'; // Pas d'effet de zoom en mode mobile
-            body.style.overflow = 'auto'; // Activer le défilement de la page
-
-            return; // Quitter la fonction
+            container.style.transform = 'scale(1)';
+            body.style.overflow = 'scroll';
+            return;
         }
 
-        // Calculer le ratio de défilement
         let scrollRatio = container.scrollTop / (container.scrollHeight - container.clientHeight);
-
-        // Appliquer une transformation d'échelle
-        let scaleValue = 1 + scrollRatio * 0.2; // Ajuster cette valeur
+        let scaleValue = 1 + scrollRatio * 0.1;
         container.style.transform = `scale(${scaleValue})`;
+
+        if (container.scrollTop === 0 || container.scrollTop + container.clientHeight >= container.scrollHeight) {
+            body.style.overflow = 'auto';
+        } else {
+            body.style.overflow = 'hidden';
+        }
     }
 
+    function onScroll() {
+        applyScale();
+    }
 });
